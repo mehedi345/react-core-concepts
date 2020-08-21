85d32266a63ab946331330675bef20a25a5d3192
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-21 00:17:11 -0700

    practicing components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks =['Sakib', 'Joshim', 'Salman shah']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-       
+        <p>I am a react person</p>
+        <Person name={nayoks[0]} nayika="bubli"></Person>
+        <Person name={nayoks[1]} nayika="moushumi"></Person>
+        <Person name={nayoks[2]} nayika="shabnoor"></Person>
       </header>
     </div>
   );
+}
+function Person(props){
+
+  const personStyle = {
+    border: '2px solid red',
+    margin: '10px',
+    padding: '10px',
+    borderRadius: '10px'
+  }
+   return (
+     <div style={personStyle}>
+       <h1>name: {props.name}</h1>
+        <h2>hero of {props.nayika}</h2>
+      </div>)
+  
 }
 
 export default App;
